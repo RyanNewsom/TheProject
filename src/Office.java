5fd40d07b67fd25fdc6819989942dea66f509e07
RyanNewsom <rnewsom@msudenver.edu>
2015-04-16 09:35:40 -0600

    I added an awesome workaholics image for the GUI background. Tell me what you guys think! Not done with the title yet, probably going to add some cool wordart type font.

--- src/Office.java
+++ src/Office.java
@@ -2,6 +2,13 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,29 +20,46 @@ import javax.swing.JTextField;
  * @version 1
  */
 public class Office {
+	static Image background;
 	public static void main(String[]args){
+		Office office = new Office();
+	}
+	public Office(){
 		JFrame frame = new JFrame("Just another day at the office");
-		JPanel panel = new JPanel();
 		JLabel title = new JLabel("Please enter the time for the office simulation in minutes");
+		File file = new File("workaholics.jpg");
 		JTextField titleInput = new JTextField(20);
 		JButton go = new JButton("GO");
-		Font largeFont = new Font(null, 0, 22);
-		frame.setBounds(350,350,600,600);
+		Font largeFont = new Font("Verdana", 0, 30);
+		//frame.setResizable(false);
+		try {
+			background = ImageIO.read(file);
+		} catch (IOException e) {
+			System.out.println("Image load failed");
+			// TODO Auto-generated catch block;
+		}
+		final JPanel j = new IPanel(background);
+		frame.setBounds(350,350,1080,700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(false);
-		panel.setBackground(Color.gray);
-		panel.setLayout(new GridBagLayout());
+		frame.add(j);
 		title.setFont(largeFont);
+		title.setForeground(Color.BLUE);
 		go.setFont(largeFont);
+		j.setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
+		go.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				//RESERVED will initiate running the program for X minutes
+			}	
+		});
 		//////////////Row 0//////////////////////
 		gbc.weightx = 0.1;
 		gbc.weighty = 0.15;
 		gbc.gridx = 0;
 		gbc.gridy = 0;
 		gbc.gridwidth = 0;
-		gbc.anchor = GridBagConstraints.SOUTH;
-		panel.add(title,gbc);
+		gbc.anchor = GridBagConstraints.CENTER;
+		j.add(title,gbc);
 		//////////////Row 1//////////////////////
 		gbc.weightx = 0.1;
 		gbc.weighty = 0.15;
@@ -43,7 +67,7 @@ public class Office {
 		gbc.gridy = 1;
 		gbc.gridwidth = 0;
 		gbc.anchor = GridBagConstraints.CENTER;
-		panel.add(titleInput,gbc);
+		j.add(titleInput,gbc);
 		//////////////Row 2//////////////////////
 		gbc.weightx = 0.1;
 		gbc.weighty = 0.15;
@@ -51,10 +75,10 @@ public class Office {
 		gbc.gridy = 2;
 		gbc.gridwidth = 0;
 		gbc.anchor = GridBagConstraints.NORTH;
-		panel.add(go,gbc);
+		j.add(go,gbc);
 		
-		frame.add(panel);
+		frame.repaint();
 		frame.setVisible(true);
 	}
-
 }
+
