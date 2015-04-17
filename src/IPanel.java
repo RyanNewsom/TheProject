f718b362dd3e97079cb3f232b08bb8474911fb54
Kriswest56 <broncosfreak56@gmail.com>
2015-04-16 19:57:05 -0600

    Testing changes to Secretary

--- src/IPanel.java
+++ src/IPanel.java
@@ -14,7 +14,7 @@ private Image             imageOrg         = null;
 private Image             image            = null;
 {
     addComponentListener(new ComponentAdapter() {
-        @Override
+        //@Override
         public void componentResized(final ComponentEvent e) {
             final int w = IPanel.this.getWidth();
             final int h = IPanel.this.getHeight();
@@ -29,7 +29,7 @@ public IPanel(final Image i) {
     image = i;
 }
 
-@Override
+//@Override
 public void paintComponent(final Graphics g) {
     super.paintComponent(g);
     if (image != null)
--- src/Secratery.java
+++ src/Secratery.java
@@ -1,4 +1,11 @@
 
 public class Secratery {
+	
+	private void customerResponse(int linkedListSize){
+		if(linkedListSize < 10){
+			System.out.println("Next please!");
+		}
+		
+	}
 
 }
