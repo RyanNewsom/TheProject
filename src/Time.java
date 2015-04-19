8ce46c5e76a9b87bee2414f9d454150b836273fe
Kriswest56 <broncosfreak56@gmail.com>
2015-04-19 03:13:18 -0600

    Made remove duplicated loop

--- src/Time.java
+++ src/Time.java
@@ -142,19 +142,25 @@ public class Time {
 			
 		}
 		
-		for(int i = 0; i<customersComplete.size()-1; i++){
-			
+		//sort list
+		for(int i = 0; i<customersComplete.size()-1; i++){			
 			Customer curr = customersComplete.get(i);
 			Customer next = customersComplete.get(i+1);
-			if(curr.getStartTimeF() == next.getStartTimeF()){
-				customersComplete.remove(i);
-			}
 			if(curr.getAnswerTime() > next.getAnswerTime()){
 				customersComplete.add(i, next);
 				customersComplete.add(i+1, curr);
 				break;
 			}
 			
+		}
+		
+		//remove duplicates
+		for(int i = 0; i<customersComplete.size()-1; i++){
+			Customer curr = customersComplete.get(i);
+			Customer next = customersComplete.get(i+1);
+			if(curr.getAnswerTime() == next.getAnswerTime()){
+				customersComplete.remove(i+1);
+			}
 		}
 		
 		
